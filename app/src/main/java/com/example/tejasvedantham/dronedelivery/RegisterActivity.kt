a3a17c0ca23d41a28ff8498d678846238a576c45
Caden Farley <cfarley6@gatech.edu>
2021-10-11 20:58:45 -0400

    added Firebase integration and registration code

--- app/src/main/java/com/example/tejasvedantham/dronedelivery/RegisterActivity.kt
+++ app/src/main/java/com/example/tejasvedantham/dronedelivery/RegisterActivity.kt
@@ -1,22 +1,40 @@
 package com.example.tejasvedantham.dronedelivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var usernameField: EditText
     lateinit var passwordField: EditText
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         usernameField = findViewById(R.id.r_usernameField)
         passwordField = findViewById(R.id.r_passwordField)
+
+        auth = Firebase.auth
     }
 
     fun register(v: View?) {
+        auth.createUserWithEmailAndPassword(usernameField.text.toString(), passwordField.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "Registration Succeeded", Toast.LENGTH_SHORT).show()
 
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "Registration Failed: " + task.exception.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
