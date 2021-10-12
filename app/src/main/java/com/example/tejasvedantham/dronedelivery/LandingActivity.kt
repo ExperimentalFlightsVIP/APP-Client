ef424df9ca52359159f8d0858d8503e63b9a18e9
Rayavaram, Snehith Swamisai <srayavaram3@gatech.edu>
2021-10-11 20:16:18 -0500

    updated latest commit with login functionality, instead of older commit

--- app/src/main/java/com/example/tejasvedantham/dronedelivery/LandingActivity.kt
+++ app/src/main/java/com/example/tejasvedantham/dronedelivery/LandingActivity.kt
@@ -1,25 +1,53 @@
 package com.example.tejasvedantham.dronedelivery
 
 import android.content.Intent
+import android.accounts.AccountManager
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.security.AccessController.getContext
 
 class LandingActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
         val logoImage = findViewById<View>(R.id.logoView) as ImageView
         logoImage.setImageResource(R.drawable.logo)
         logoImage.adjustViewBounds = true
+        auth = Firebase.auth
     }
 
     fun login(v: View?) {
-        Snackbar.make(v!!, "Login Successful", Snackbar.LENGTH_LONG).show()
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        val user_name_area = findViewById<EditText>(R.id.usernameField)
+        val password_area = findViewById<EditText>(R.id.passwordField)
+        val username = user_name_area.text
+        val password = password_area.text
+        Log.i("username", username.toString())
+        Log.i("password", password.toString())
+        auth.signInWithEmailAndPassword(username.toString(), password.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, move on to UI/maps section
+                    Snackbar.make(v!!, "Login Successful", Snackbar.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, do nothing
+                }
+            }
+        // [END sign_in_with_email]
     }
 
     fun register(v: View?) {
